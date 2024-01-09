685de2b20dcb1996cea11accf1cfd03b5597b21b
blueq <85065731+ducquanpham@users.noreply.github.com>
2024-01-09 10:51:54 +0700

    Update main.js
      Em quay trúng thì chụp màn gửi chị nha .

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ function closeModal() {
 function checkPassword() {
     var password = document.getElementById('passwordInput').value;
     // pass
-    if (password === 'KH09011') {
+    if (password === 'KH09012') {
         $('.wheel__button').prop('disabled', false);
         alert('Chúc mừng bạn đăng nhập thành công.');
         closeModal();
