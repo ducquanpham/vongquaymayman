c4f80c0d4cb50d44dcffd969e060b784efd6eb86
blueq <85065731+ducquanpham@users.noreply.github.com>
2024-01-10 10:19:21 +0700

    Update main.js
     ᴋếᴛ ʙạɴ ᴠà ɴʜắɴ ᴛɪɴ ᴄʜᴏ ᴄʜị ɴʜậɴ ǫᴜà ɴʜᴀ <3

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ function closeModal() {
 function checkPassword() {
     var password = document.getElementById('passwordInput').value;
     // pass
-    if (password === 'KH09013') {
+    if (password === 'KH10011') {
         $('.wheel__button').prop('disabled', false);
         alert('Chúc mừng bạn đăng nhập thành công.');
         closeModal();
